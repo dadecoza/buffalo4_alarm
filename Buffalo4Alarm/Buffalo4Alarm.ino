6fae593457f253c76b65cafcdf8a79adce527e6d
Le Roux <Johannes.LeRoux@vodacom.co.za>
2020-01-06 06:59:07 +0200

    initial commit

---
+++ Buffalo4Alarm/Buffalo4Alarm.ino
@@ -0,0 +1,58 @@
+#include "EspMQTTClient.h"
+
+//define where your pins are
+int latchPin = 0; //grys
+int dataPin = 3; //swart
+int clockPin = 2; //wit 
+
+byte switchVar1 = 0;
+byte prevVar1 = 0;
+
+unsigned long previousMillis = 0;
+const long interval = 10000;
+
+EspMQTTClient client("Buffalo4", "*****", "192.168.3.39", "", "", "buffalo4Alarm", 1883);
+
+void setup()
+{
+  Serial.begin(115200,SERIAL_8N1,SERIAL_TX_ONLY); 
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT); 
+  pinMode(dataPin, INPUT);
+}
+
+void onConnectionEstablished() {}
+
+void loop()
+{
+  digitalWrite(latchPin,1);
+  delayMicroseconds(20);
+  digitalWrite(latchPin,0);
+  switchVar1 = shiftIn(dataPin, clockPin);
+
+  unsigned long currentMillis = millis();
+  
+  if ((currentMillis - previousMillis >= interval)||(switchVar1 != prevVar1)) {
+    previousMillis = currentMillis;
+    client.publish("buffalo4/alarm", String(switchVar1));
+    prevVar1 = switchVar1;
+  }
+  client.loop();
+}
+
+byte shiftIn(int myDataPin, int myClockPin) { 
+  int i;
+  int temp = 0;
+  byte myDataIn = 0;
+  for (i=7; i>=0; i--)
+  {
+    digitalWrite(myClockPin, 0);
+    delayMicroseconds(0.2);
+    temp = digitalRead(myDataPin);
+    if (temp) {
+      myDataIn = myDataIn | (1 << i);
+    }
+    digitalWrite(myClockPin, 1);
+  }
+  return myDataIn;
+}
